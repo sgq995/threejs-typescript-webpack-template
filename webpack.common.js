8478d9135ec17e173e67b5090af372078f769126
Sebastian G. Q <sgq995@gmail.com>
2021-02-14 11:33:27 -0500

    chore: Create development and production config

---
+++ webpack.common.js
@@ -0,0 +1,47 @@
+const path = require('path');
+
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[hash].bundle.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+
+      {
+        test: /\.xml/i,
+        use: ['xml-loader'],
+      },
+    ],
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      template: path.resolve(__dirname, 'public/index.html'),
+    }),
+  ],
+};
---
+++ webpack.dev.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    hot: true,
+  },
+
+  optimization: {
+    usedExports: true,
+  },
+});
